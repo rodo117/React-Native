0726b0314bfe1906876bb62a2f788fecc25d97bf
Rodolfo Melin <rmelin@luxoft.com>
2018-09-26 16:19:13 -0500

    Side Drawer Navigation Implemented

--- confusion/components/MainComponent.js
+++ confusion/components/MainComponent.js
@@ -1,34 +1,67 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Menu from './MenuComponent'
-import {DISHES} from '../shared/dishes'
+import Home from './HomeComponent'
 import Dishdetail from './DishDetailComponent';
-import {View, Platform} from 'react-native';
-import {createStackNavigator} from 'react-navigation';
+import { View, Platform } from 'react-native';
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
 
-const MenuNavigator = createStackNavigator ({
-    Menu: {screen: Menu}, 
-    Dishdetail: {screen :Dishdetail}
+const MenuNavigator = createStackNavigator({
+    Menu: { screen: Menu },
+    Dishdetail: { screen: Dishdetail }
 }, {
-   initialRouteName: 'Menu', 
-   navigationOptions : {
-      headerStyle:{
-          backgroundColor: '#512DA8'
-      },
-      headerTintColor:'#fff',
-      headerTitleStyle:{
-          color: '#fff'
-      }
-   }
-})
+        initialRouteName: 'Menu',
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#512DA8'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    });
+
+const HomeNavigator = createStackNavigator({
+    Home: { screen: Home },
+}, {
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#512DA8'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    });
+
+const MainNavigator = createDrawerNavigator({
+    Home: {
+        screen: HomeNavigator,
+        navigationOptions: {
+            tittle: 'Home',
+            drawLabel: 'Home'
+        }
+    },
+    Menu: {
+        screen: MenuNavigator,
+        navigationOptions: {
+            tittle: 'Menu',
+            drawLabel: 'Menu'
+        }
+    }
+},{
+    drawerBackgroundColor:'#D1C4E9'
+});
 
 class Main extends Component {
-   render () {
-       return(
-           <View style = {{flex:1, paddingTop: Platform.OS==='ios'? 0 : Expo.Constants.statusBarHeight }}>
-           <MenuNavigator/>
-           </View>
-       );
-   }
+    render() {
+        return (
+            <View style={{ flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
+                <MainNavigator/>
+            </View>
+        );
+    }
 }
 
 export default Main;
