174f428758c7dcabdfdd391b3c7c48b1d10b375c
Rodolfo Melin <rmelin@luxoft.com>
2018-10-02 15:07:55 -0500

    Setting icons to left menu and set icon to stack menu

--- confusion/components/MainComponent.js
+++ confusion/components/MainComponent.js
@@ -2,66 +2,136 @@ import React, { Component } from 'react';
 import Menu from './MenuComponent'
 import Home from './HomeComponent'
 import Dishdetail from './DishDetailComponent';
-import { View, Platform } from 'react-native';
-import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
+import { View, Platform, Image, StyleSheet, ScrollView, Text } from 'react-native';
+import { createStackNavigator, createDrawerNavigator, DrawerItems, SafeAreaView } from 'react-navigation';
+import { Icon } from 'react-native-elements';
 
 const MenuNavigator = createStackNavigator({
     Menu: { screen: Menu },
     Dishdetail: { screen: Dishdetail }
 }, {
         initialRouteName: 'Menu',
-        navigationOptions: {
+        navigationOptions: ({ navigation }) => ({
+            headerLeft: <Icon name='menu' size={24}
+                color='white'
+                onPress={() => navigation.toggleDrawer()}
+            />,
             headerStyle: {
                 backgroundColor: '#512DA8'
             },
             headerTintColor: '#fff',
             headerTitleStyle: {
                 color: '#fff'
-            }
-        }
+            },
+        })
     });
 
 const HomeNavigator = createStackNavigator({
     Home: { screen: Home },
 }, {
-        navigationOptions: {
+        navigationOptions: ({ navigation }) => ({
             headerStyle: {
                 backgroundColor: '#512DA8'
             },
             headerTintColor: '#fff',
             headerTitleStyle: {
                 color: '#fff'
-            }
-        }
+            },
+            headerLeft: <Icon name='menu' size={24}
+                color='white'
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
     });
+
+const CustomDrawerContentComponent = (props) => (
+    <ScrollView>
+        <SafeAreaView style={styles.container}
+            forceInset={{ top: 'always', horizontal: 'never' }}>
+            <View style={styles.drawerHeader}>
+                <View style={{ flex: 1 }}>
+                    <Image source={require('./images/alberto.png')} style={styles.drawerImage}>
+                    </Image>
+                </View>
+                <View style={{ flex: 2 }}>
+                    <Text style={styles.drawerHeaderText}> Ristorante ConFusion</Text>
+                </View>
+            </View>
+            <DrawerItems {...props} />
+        </SafeAreaView>
+    </ScrollView>
+
+);
 
 const MainNavigator = createDrawerNavigator({
     Home: {
         screen: HomeNavigator,
         navigationOptions: {
-            tittle: 'Home',
-            drawLabel: 'Home'
+            title: 'Home',
+            drawLabel: 'Home',
+            drawerIcon: ({ tintColor }) => (
+                <Icon
+                    name='home'
+                    type='font-awesome'
+                    size={24}
+                    color={tintColor}
+                />
+            )
         }
     },
     Menu: {
         screen: MenuNavigator,
         navigationOptions: {
-            tittle: 'Menu',
-            drawLabel: 'Menu'
+            title: 'Menu',
+            drawLabel: 'Menu',
+            drawerIcon: ({ tintColor }) => (
+                <Icon
+                    name='list'
+                    type='font-awesome'
+                    size={22}
+                    color={tintColor}
+                />
+            )
         }
     }
-},{
-    drawerBackgroundColor:'#D1C4E9'
-});
+}, {
+        drawerBackgroundColor: '#D1C4E9',
+        contentComponent: CustomDrawerContentComponent
+
+    });
 
 class Main extends Component {
     render() {
         return (
             <View style={{ flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
-                <MainNavigator/>
+                <MainNavigator />
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    drawerHeader: {
+        backgroundColor: '#512DA8',
+        height: 140,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row'
+    },
+    drawerHeaderText: {
+        color: 'white',
+        fontSize: 22,
+        fontWeight: 'bold'
+    },
+    drawerImage: {
+        margin: 10,
+        width: 80,
+        height: 60
+    }
+});
 
 export default Main;
